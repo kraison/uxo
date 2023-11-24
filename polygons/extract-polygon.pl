395078a63508ea5b275061bfb36eeba0a541a6c7
Kevin Raison <kevin@bluestarsunflower.org>
2023-11-24 16:45:14 +0200

    First pass at getting bounding boxes from Dronelink mission data.

---
+++ polygons/extract-polygon.pl
@@ -0,0 +1,85 @@
+#!/usr/bin/perl
+#
+# This Will Load a Drontlink Mission File ( ARGV[0] )
+# The Result Is A KML File ( mission-polygon.kml )
+# Extracting the bounding box from the mission file
+#
+
+use strict;
+use warnings;
+use XML::LibXML;
+use Data::Dumper;
+use HTML::Template;
+use List::Util qw(min);
+use List::MoreUtils qw( minmax );
+
+# Read the KML file
+my $kml_file = $ARGV[0];
+my $parser = XML::LibXML->new();
+my $doc = $parser->parse_file($kml_file);
+
+# Find all coordinates within the KML file and extract MinX, MinY, MaxX and MaxY
+my @coordinates;
+my(@latitudes,@longitudes);
+
+foreach my $placemark ($doc->findnodes("//*[local-name()='Placemark']")) {
+    my $desc = ($placemark->findnodes("./*[local-name()='description']")->get_node(1));
+    next unless $desc;
+    if($desc->textContent eq 'Single Photo') {
+        my $point_node = $placemark->findnodes("./*[local-name()='Point']/*[local-name()='coordinates']")->get_node(1);
+        next unless $point_node;
+
+        my $coordinates_node = $point_node->textContent;
+
+        # Split the coordinates string and store them in an array
+        my @coords = map { [split /,\s*/, $_] } split /\n/, $coordinates_node;
+        foreach my $coord (@coords) {
+            my ($lon, $lat) = @$coord;
+            if (defined $lon && defined $lat) {
+                push(@latitudes,$lat);
+                push(@longitudes,$lon);
+
+                push(@coordinates, { LAT => $lat, LON => $lon });
+            }
+        }
+    }
+}
+
+## Simple bounding box using synthetic coordinates
+my($min_lat, $max_lat) = minmax(@latitudes);
+my($min_lon, $max_lon) = minmax(@longitudes);
+
+my $template = HTML::Template->new(filename => 'placemark.tmpl');
+$template->param(MINLAT => $min_lat);
+$template->param(MAXLAT => $max_lat);
+$template->param(MINLON => $min_lon);
+$template->param(MAXLON => $max_lon);
+
+## Find the vertexes of the polygon;  we assume 4 for now; that's not a great assumption
+my @outside_coords;
+foreach my $c (@coordinates) {
+    if($c->{'LAT'} == $min_lat or $c->{'LAT'} == $max_lat or $c->{'LON'} == $min_lon or $c->{'LON'} == $max_lon) {
+        push(@outside_coords,$c);
+    }
+}
+sub coord_sort {
+    #if($a->{'LAT'} <= $b->{'LAT'} and $a->{'LON'} <= $b->{'LON'}) {
+    if($a->{'LAT'} <= $b->{'LAT'}) {
+        return(1);
+    }
+}
+@outside_coords = sort coord_sort @outside_coords;
+$template->param(LAT1 => $outside_coords[0]->{'LAT'});
+$template->param(LAT2 => $outside_coords[1]->{'LAT'});
+$template->param(LAT3 => $outside_coords[2]->{'LAT'});
+$template->param(LAT4 => $outside_coords[3]->{'LAT'});
+$template->param(LON1 => $outside_coords[0]->{'LON'});
+$template->param(LON2 => $outside_coords[1]->{'LON'});
+$template->param(LON3 => $outside_coords[2]->{'LON'});
+$template->param(LON4 => $outside_coords[3]->{'LON'});
+
+## Write the kml to STDOUT for now.
+print $template->output;
+
+exit(0);
+
